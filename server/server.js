a1daff0375100ab217bb9d04e69733f8a0ac2083
Gizem Cetinkaya <gizemce00@gmail.com>
2023-03-02 14:57:48 -0500

    button added

--- server/server.js
+++ server/server.js
@@ -3,14 +3,23 @@ const cors = require("cors")
 const path = require("path")
 
 const app = express()
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'fd142cac0b6c4baa8e0082edadc99bce',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static("public"))
 
+const princess = ['Elizabeth', 'Diana', 'Victoria']
+rollbar.log("site is visited")
 
 app.get('/',(req,res)=>{
     res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
+    
 })
 app.get('/css',(req,res)=>{
     res.status(200).sendFile(path.join(__dirname, '../public/styles.css'))
