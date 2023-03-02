26b6a5e0322326d339b7732a3872f941cb0fbe67
Gizem Cetinkaya <gizemce00@gmail.com>
2023-03-02 16:52:33 -0500

    axios added

--- public/main.js
+++ public/main.js
@@ -2,4 +2,15 @@ let castle = document.querySelector("img")
 
 castle.addEventListener("click",(evt)=>{
     alert("this is my castle, there are many like it, but this one is mine")
+    axios.get('/rollbar').then(response => {
+
+    }).catch(err => console.log(err))
+})
+
+let button = document.querySelector("button")
+
+button.addEventListener("click",(evt)=>{
+    axios.get('/button').then(response => {
+
+    }).catch(err => console.log(err))
 })
--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ var rollbar = new Rollbar({
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
-princess = ["gizem","gamze"]
 app.use(express.json())
 app.use(cors())
 app.use(express.static("public"))
@@ -18,17 +17,28 @@ app.use(express.static("public"))
 
 app.get('/',(req,res)=>{
     res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
-    rollbar.log("site is visited")
+    
     
 })
 app.get('/css',(req,res)=>{
     res.status(200).sendFile(path.join(__dirname, '../public/styles.css'))
+    
+    
 })
 app.get('/js',(req,res)=>{
     res.status(200).sendFile(path.join(__dirname, '../public/main.js'))
+    
+    
 
 })
-
+app.get('/rollbar', (req,res) => {
+    res.sendStatus(200)
+    rollbar.log('site visited')
+})
+app.get('/button',(req,res)=>{
+    res.sendStatus(200)
+    rollbar.info("button is clicked")
+})
 
 
 
